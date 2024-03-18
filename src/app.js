07fbf1f662b77b9ed47ddee9b143115b7e08e9f7
Abhay Kumar Mishra <mishrakumar.abhay1@gmail.com>
2024-03-18 16:05:12 +0530

    All setup configured

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import { express } from "express";
+import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser";
 
@@ -13,5 +13,11 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+// Routes Import
+import userRouter from "./routes/user.routes.js"
+
+// Routes Declaration
+app.use("/api/v1/users", userRouter)
 
 export { app }
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,6 +2,8 @@ import { asyncHandler } from "../utils/asyncHandler.js"
 
 const registerUser = asyncHandler( async (req, res) => {
     res.status(200).json({
-        message: "ok"
+        message: "Chai aur code"
     })
 })
+
+export { registerUser }
