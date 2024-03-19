4a4f7d04a0f6d059c674b3fdcbc7894202eda8ac
Abhay Kumar Mishra <mishrakumar.abhay1@gmail.com>
2024-03-19 15:32:46 +0530

    completed user registration api

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -20,7 +20,7 @@ const uploadOnCloudinary = async (localFilePath) => {
         )
 
         // File has uploaded successfully
-        console.log("File is uploaded on cloudinary ", response.url);
+        fs.unlinkSync(localFilePath)
         return response;
     } catch (error) {
         fs.unlinkSync(localFilePath)  // Remove the locally saved temporary file as the upload operation got failed
